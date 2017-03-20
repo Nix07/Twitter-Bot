d27b069aee2c9c49b7418f69b0fa459fcaf0c66c
Nikhil Prakash <Nikhil Prakash>
2017-03-20 16:18:51 +0530

    Added access tokens in config.js file

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 console.log("This is working!");
 
 var Twit = require('twit');
+var config = require('./config');
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var T = new Twit(config);

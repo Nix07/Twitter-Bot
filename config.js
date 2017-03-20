e617a191dc93f89b3609ddfa102bda3a487b4b51
Nikhil Prakash <Nikhil Prakash>
2017-03-20 21:25:42 +0530

    Removed Access Keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  consumer_key:         'CQzE7fTCwHHJI4wpNN6h1Yow1',
-  consumer_secret:      'hCw2d6kvpuxI6qI75cacf4hQyGVY34TajDGs1ekHFJnKXRWcex',
-  access_token:         '834030478042738689-GBmPCAdpt7Penozmwwl4gIo7hIqsbVL',
-  access_token_secret:  'jeMy386NT6Q0zHq0pQrvfRXnMVLlmZ6fxx2b9lI5A3Fnl',
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
   //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 } 

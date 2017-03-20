322e18b976afc18e948d7cea052f4ede13e5e89d
Nikhil Prakash <Nikhil Prakash>
2017-03-20 17:55:49 +0530

    Post a tweet functionality added!

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-console.log("This is working!");
-
 var Twit = require('twit');
-var config = require('./config');
+var config = require('./config'); //Contains Credentials
 
 var T = new Twit(config);
 
+/* Get Resquest from the Twitter
 var params = {
 	q: 'HelloCloudSweepstakes',
 	count: 5
@@ -16,4 +15,20 @@ function gotData(err, data, response) {
 	var tweets = data.statuses;
 	for( var i = 0; i < tweets.length; i++)
 		console.log(tweets[i].text);
+}
+*/
+
+//Post Request 
+var tweet = {
+	status: "Tweeted from Twitter API using node.js! @nikhil07prakash "
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+	if(err) {
+		console.log("Something went wrong!");
+	} else {
+		console.log("It Worked!");
+	}
 }

4f1bfaad69fd58ff99b42f42ade4345c484162a1
Nikhil Prakash <Nikhil Prakash>
2017-03-20 21:23:58 +0530

    Added Response Functionality to the Bot

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+console.log("It started working!");
+
 var Twit = require('twit');
 var config = require('./config'); //Contains Credentials
 
 var T = new Twit(config);
+
+var stream = T.stream('user'); //Using User Stream
+stream.on('follow', followed);
+
+function followed(event) {
+	var name = event.source.name;
+	var screenName = event.source.screen_name;
+	tweetIt('Thanks for following me! ' + '@' + screenName);
+}
+
+//Post Request 
+function tweetIt(txt) {
+	var tweet = {
+		status: txt
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if(err) {
+			console.log("Something went wrong!");
+		} else {
+			console.log("It Worked!");
+		}
+	}
+}
 
 /* Get Resquest from the Twitter
 var params = {
@@ -17,18 +45,3 @@ function gotData(err, data, response) {
 		console.log(tweets[i].text);
 }
 */
-
-//Post Request 
-var tweet = {
-	status: "Tweeted from Twitter API using node.js! @nikhil07prakash "
-}
-
-T.post('statuses/update', tweet, tweeted);
-
-function tweeted(err, data, response) {
-	if(err) {
-		console.log("Something went wrong!");
-	} else {
-		console.log("It Worked!");
-	}
-}
